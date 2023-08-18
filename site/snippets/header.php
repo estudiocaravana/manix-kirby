3a42b1b1b31603b8283a91d51ab8216bca05912a
edupoch <edupoch@gmail.com>
2023-08-18 17:55:29 +0200

    Ajustamos el title del sitio

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $site->title() ?> | <?= $page->title() ?></title>
+    <title>Café Mánix</title>
     <meta name="description" content="<?= $site->description() ?>">
     <!-- fuente Poppins -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
