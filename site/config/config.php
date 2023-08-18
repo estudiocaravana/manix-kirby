d1e3cdcb7cbfbb55a845b81de507dec746d888e2
edupoch <edupoch@gmail.com>
2023-08-18 17:51:10 +0200

    Docker + Static Site Generator + Github Actions deploy

--- site/config/config.php
+++ site/config/config.php
@@ -2,4 +2,10 @@
 return [
     'debug' => true,
     'languages' => true,
+    'd4l' => [
+        'static_site_generator' => [
+            'endpoint' => 'generate-static-site',
+            // 'base_url' => 'http://localhost:8888'
+        ]
+    ]
 ];
