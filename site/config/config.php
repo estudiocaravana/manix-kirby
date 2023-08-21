123487c9f7be3fb4b25e2c01941099bb748b6c43
edupoch <edupoch@gmail.com>
2023-08-21 13:25:06 +0200

    Añadimos explícitamente /carta en la configuración del generador de web estática

--- site/config/config.php
+++ site/config/config.php
@@ -5,6 +5,10 @@ return [
     'd4l' => [
         'static_site_generator' => [
             'endpoint' => 'generate-static-site',
+            'custom_routes' =>  [[
+                'path' => 'carta',
+                'page' => 'carta'
+            ]],
             // 'base_url' => 'http://localhost:8888'
         ]
     ]
